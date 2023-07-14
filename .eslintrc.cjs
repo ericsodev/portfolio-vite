918c9375e74eb2e05193746e727a2288435eb1c7
Eric <eric.so.dev@gmail.com>
2023-07-14 18:49:48 -0400

    unused variable linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,18 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": "warn",
     "@typescript-eslint/ban-ts-comment": 0,
+    "no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+        varsIgnorePattern: "([iI]gnored)|([tT]mp)|([tT]emp)",
+      },
+    ],
   },
   reportUnusedDisableDirectives: true,
-  ignorePatterns: ["dist/*"],
+  ignorePatterns: ["dist/*", "src/__generated__/*"],
   env: { browser: true, es2020: true, node: true },
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "detect" } },
