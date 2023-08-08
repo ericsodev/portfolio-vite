844676aa243be6717488f910ac5099724cfef9a6
Eric <eric.so.dev@gmail.com>
2023-08-08 16:05:06 -0400

    add modals for projects

--- pages/index/components/ProjectCard.tsx
+++ pages/index/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { Props } from "../index.page.server";
 import { motion } from "framer-motion";
 //@ts-ignore
@@ -7,10 +7,11 @@ import WebIcon from "~icons/mdi/web";
 import ExpandIcon from "~icons/mdi/arrow-expand";
 // @ts-ignore
 import GithubIcon from "~icons/simple-icons/github";
+import ProjectModal from "./ProjectModal";
 
-type Project = NonNullable<Props["project"]>[0];
+export type Project = NonNullable<Props["project"]>[0];
 
-const IconComponent: React.FC<{ Icon: React.FC<any> }> = ({ Icon }) => {
+export const IconComponent: React.FC<{ Icon: React.FC<any> }> = ({ Icon }) => {
   return (
     <Icon
       className={`after:text-lg hover:text-tealgreen transition-all hover:scale-125`}
@@ -19,12 +20,34 @@ const IconComponent: React.FC<{ Icon: React.FC<any> }> = ({ Icon }) => {
 };
 
 export default function ProjectCard({ project }: { project: Project }) {
+  const [modalOpen, setModalOpen] = useState(false);
+  useEffect(() => {
+    if (!modalOpen) {
+      document.body.style.overflow = "scroll";
+      return;
+    }
+    document.body.style.overflow = "hidden";
+  }, [modalOpen]);
+
   return (
-    <motion.div className="cursor-pointer group bg-darkgray/60 p-6 rounded-md flex flex-col gap-4 border-stone-800 hover:border-purple/20 border-[1px]">
+    <motion.div className="group bg-darkgray/60 p-6 rounded-md flex flex-col gap-4 border-stone-800 hover:border-purple/20 border-[1px]">
+      {modalOpen && (
+        <ProjectModal project={project} setOpen={setModalOpen}></ProjectModal>
+      )}
       <div className="flex items-center">
         <h1 className="text-white text-xl font-semibold">{project.name}</h1>
         {project.long_desc && (
-          <ExpandIcon className="text-xl ml-auto text-offwhite/50 hover:text-tealgreen opacity-0 transition group-hover:opacity-100"></ExpandIcon>
+          <motion.div
+            onClick={() => {
+              setModalOpen(true);
+            }}
+            transition={{ type: "spring", bounce: 0.8 }}
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.3 }}
+            className="ml-auto group-hover:opacity-100 opacity-50 transition-opacity"
+          >
+            <ExpandIcon className="text-xl ml-auto text-offwhite/50 hover:text-tealgreen transition-colors"></ExpandIcon>
+          </motion.div>
         )}
       </div>
       <div className="flex gap-2">
@@ -46,7 +69,7 @@ export default function ProjectCard({ project }: { project: Project }) {
           .map((skill, i) => (
             <div
               key={skill?.name + "" + i}
-              className="px-2 py-1 rounded-md bg-purple/70 text-sm text-white"
+              className="px-2 py-1 rounded-md bg-purple/70 text-xs text-white"
             >
               {skill?.name}
             </div>
--- pages/index/components/ProjectModal.tsx
+++ pages/index/components/ProjectModal.tsx
@@ -1,6 +1,94 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { IconComponent, Project } from "./ProjectCard";
+import ReactMarkdown from "react-markdown";
+import mdStyle from "../styling/markdown.module.css";
 
-export default function ProjectModal({project}: {project: Proejct}) {
-  return <motion.div></motion.div>;
+//@ts-ignore
+import WebIcon from "~icons/mdi/web";
+//@ts-ignore
+import GithubIcon from "~icons/simple-icons/github";
+
+type Props = {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  project: Project;
+};
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+export default function ProjectModal({ project, ...props }: Props) {
+  return (
+    <Backdrop {...props}>
+      <motion.div
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        onClick={(e) => e.stopPropagation()}
+        className="group w-fit bg-darkgray p-6 rounded-md flex flex-col gap-4 border-stone-800 hover:border-purple/20 border-[1px]"
+      >
+        <h1 className="text-offwhite font-semibold text-xl">{project.name}</h1>
+        <div className="flex gap-2">
+          {project.github && (
+            <a href={project.github} className="text-offwhite">
+              <IconComponent Icon={GithubIcon}></IconComponent>
+            </a>
+          )}
+          {project.website && (
+            <a href={project.website} className="text-offwhite">
+              <IconComponent Icon={WebIcon}></IconComponent>
+            </a>
+          )}
+        </div>
+        <div className="flex gap-2 mt-auto">
+          {project.skills
+            ?.filter((x) => !!x)
+            .map((skill, i) => (
+              <div
+                key={skill?.name + "" + i}
+                className="px-1.5 py-0.5 rounded-md bg-purple/70 text-xs text-white"
+              >
+                {skill?.name}
+              </div>
+            ))}
+        </div>
+        <div className="max-h-96 w-fit mt-6 max-w-[75ch] text-offwhite/90 box-border text-sm leading-loose overflow-y-scroll">
+          <ReactMarkdown className={mdStyle.reactMd}>
+            {project.long_desc ?? ""}
+          </ReactMarkdown>
+        </div>
+      </motion.div>
+    </Backdrop>
+  );
+}
+
+export function Backdrop({
+  setOpen,
+  children,
+}: Pick<Props, "setOpen"> & React.PropsWithChildren) {
+  return (
+    <motion.div
+      className="fixed px-4 flex justify-center items-center z-20 top-0 right-0 bottom-0 left-0 bg-slate-950/20 backdrop-blur-[2px]"
+      onClick={() => setOpen(false)}
+    >
+      {children}
+    </motion.div>
+  );
 }
